2de762479f73b99350f5dc51b011ceff1452d30b
Dayo Salam <dayosalamboi@gmail.com>
2023-06-22 13:55:10 +0100

    Second commit

--- bookscraper/pipelines.py
+++ bookscraper/pipelines.py
@@ -13,23 +13,28 @@ class BookscraperPipeline:
         adapter = ItemAdapter(item)
 
         # Strip all whitespaces from strings
-        # field_names = adapter.field_names()
-        # for field_name in field_names:
-        #     if field_name != 'description':
-        #         value = adapter.get(field_name)
-        #         adapter[field_name] = value[0].strip()
+        field_names = adapter.field_names()
+        for field_name in field_names:
+            if field_name != 'description':
+                value = adapter.get(field_name)
+                list_value = list(value)
+                adapter[field_name] = [element.strip()
+                                       for element in list_value]
 
         # Category & Product Type to switch to lowercase
         lowercase_keys = ['category', 'product_type']
         for lowercase_key in lowercase_keys:
             value = adapter.get(lowercase_key)
-            adapter[lowercase_key] = value[0].lower()
+            list_value = list(value)
+            adapter[lowercase_key] = [element.lower()
+                                      for element in list_value]
 
         # Price is converted to float
         price_keys = ['price', 'price_excl_tax', 'price_incl_tax', 'tax']
         for price_key in price_keys:
             value = adapter.get(price_key)
-            value = value.replace('$', '')
+            list_value = list(value)
+            value = list_value.replace('$', '')
             print("***********************************************")
             print(type(value))
             adapter[price_key] = float(value)
